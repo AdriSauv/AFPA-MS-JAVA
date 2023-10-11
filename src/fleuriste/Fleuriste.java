1e4850dac96008a8171c77f73f554bf2eec91c6b
Adrien <adriensauvage59@gmail.com>
2023-10-11 11:21:08 +0200

    ajoutFLeur() added

--- src/fleuriste/Fleuriste.java
+++ src/fleuriste/Fleuriste.java
@@ -5,11 +5,16 @@ import java.util.*;
 public class Fleuriste{
 	private List<Fleur> stockFleurs = new ArrayList<>();
 	private Map<String, String> clients;
+	private Bouquet bouquetEnCours;
 	
 	
 	public Fleuriste() {
         stockFleurs = new ArrayList<>();
         clients = new HashMap<>();
+    }
+	
+	public void creationBouquet(String nom, String prenom) {
+        bouquetEnCours = new Bouquet(nom, prenom);
     }
 	
 	public int quantiteEnStock(String nomFleur) {
@@ -28,5 +33,27 @@ public class Fleuriste{
             }
         }
         return 0.0;
+    }
+	
+	public void ajoutFleur(String nomFleur, int qte) {
+        if (bouquetEnCours != null) {
+            int quantiteEnStock = quantiteEnStock(nomFleur);
+            if (quantiteEnStock >= qte) {
+                Fleur fleur = null;
+                for (Fleur f : stockFleurs) {
+                    if (f.getNom().equals(nomFleur)) {
+                        fleur = f;
+                        break;
+                    }
+                }
+                bouquetEnCours.ajouterFleur(fleur, qte);
+                // MaJ stock
+                fleur.reduireStock(qte);
+            } else {
+                System.out.println("Stock insuffisant pour la fleur " + nomFleur);
+            }
+        } else {
+            System.out.println("Aucun bouquet en cours de composition.");
+        }
     }
 }
