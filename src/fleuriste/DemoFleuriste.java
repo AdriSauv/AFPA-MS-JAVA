1cee84e9215629cf9b3da9a44d17805973e8c537
Adrien <adriensauvage59@gmail.com>
2023-10-12 08:52:35 +0200

    Test code ok, generation facture, bouquet, client ok

--- src/fleuriste/DemoFleuriste.java
+++ src/fleuriste/DemoFleuriste.java
@@ -4,16 +4,23 @@ public class DemoFleuriste {
     public static void main(String[] args) {
        
         Fleuriste fleuriste = new Fleuriste();
-
+        Fleur rose = new Fleur("Rose", 3.50f, 14);
+        Fleur tulipe = new Fleur("Tulipe", 2.21f, 18);
+        Fleur tournesol = new Fleur("Tournesol", 1.81f, 8);
+        
+        
+        Client client1 = new Client("Charlie", "Brown");
+        Bouquet bouquet = new Bouquet(client1);
         
         fleuriste.creationBouquet("Charlie", "Brown");
-
+        bouquet.ajouterFleur(tournesol, 2);
+        bouquet.ajouterFleur(rose, 5);
+        bouquet.ajouterFleur(tulipe, 3);
         
-        fleuriste.ajoutFleur("Rose", 3);
-        fleuriste.ajoutFleur("Tulipe", 5);
-
         
-        fleuriste.facturation();
+
+        System.out.println(bouquet.genererFacture());
+        
     }
 }
 
