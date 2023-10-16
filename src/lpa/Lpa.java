3a37b686f8dc3c57e0c8178cceb60c38f9ba61f6
Adrien <adriensauvage59@gmail.com>
2023-10-16 11:25:07 +0200

    nouveaux test à base de HashSet

--- src/lpa/Lpa.java
+++ src/lpa/Lpa.java
@@ -10,10 +10,9 @@ public class Lpa {
 	private Iterator<Jardin> iterJardins;
 	
 	public Lpa(ArrayList<Chocolat> lesChocolats, ArrayList<Jardin> lesJardins) {
-		this.lesJardins = lesJardins;
+		this.setLesJardins(lesJardins);
 		this.lesChocolats = lesChocolats;
 		iterJardins = lesJardins.iterator();
-		avancer();
 	}
 	
 	public void deposerChocolatEnfant(Enfant e) {
@@ -60,17 +59,29 @@ public class Lpa {
 	
 	public boolean avancer() {
 		if(getMonJardin() == null) {
+			System.out.println("LPA est arreté.");
 			return false;
 		}
 		if(iterJardins.hasNext()) {
+			monJardin = iterJardins.next();
+			System.out.println("LPA avance à l'adresse: " + monJardin.getAdresse());
 			return true;
 		} else {
 			monJardin = null;
+			System.out.println("LPA s'arrete.");
 	        return false;
 		} 
 	}
 
 	public Jardin getMonJardin() {
 		return monJardin;
+	}
+
+	public ArrayList<Jardin> getLesJardins() {
+		return lesJardins;
+	}
+
+	public void setLesJardins(ArrayList<Jardin> lesJardins) {
+		this.lesJardins = lesJardins;
 	}
 }
--- src/lpa/PascalLeLapin.java
+++ src/lpa/PascalLeLapin.java
@@ -4,44 +4,63 @@ import java.util.*;
 public class PascalLeLapin {
 	public static void main(String[] args) {
         
-		Chocolat chocolat1 = new Chocolat(Chocolat.LAPIN, 50);
-        Chocolat chocolat2 = new Chocolat(Chocolat.OEUF, 30);
-        Chocolat chocolat3 = new Chocolat(Chocolat.CLOCHE, 40);
+		Set<Lpa> mesLPA = new HashSet<>();
+		
+		Lpa lpa1 = createLPA();
+        Lpa lpa2 = createLPA();
+		
+		
+        mesLPA.add(lpa1);
+        mesLPA.add(lpa2);
+        
+        
+        for (Lpa lpa : mesLPA) {
+            runLPA(lpa);
+        }  
+		
+    }
+	
+	private static Lpa createLPA() {
+	    
+	    Chocolat chocolat1 = new Chocolat(Chocolat.LAPIN, 50);
+	    Chocolat chocolat2 = new Chocolat(Chocolat.OEUF, 30);
+	    Chocolat chocolat3 = new Chocolat(Chocolat.CLOCHE, 40);
 
-        Enfant enfant1 = new Enfant("Alice", 25);
-        Enfant enfant2 = new Enfant("Bob", 30);
-        Enfant enfant3 = new Enfant("Charlie", 35);
-        
-        Jardin jardin1 = new Jardin("142 rue Emile");
-        jardin1.ajouteEnfant(enfant1);
-        jardin1.ajouteEnfant(enfant2);
-        
-        Jardin jardin2 = new Jardin("2 rue Quasimir B");
-        jardin2.ajouteEnfant(enfant3);
-        
-        ArrayList<Chocolat> lesChocolats = new ArrayList<>();
-        lesChocolats.add(chocolat1);
-        lesChocolats.add(chocolat2);
-        lesChocolats.add(chocolat3);
-        
-        ArrayList<Jardin> lesJardins = new ArrayList<>();
-        lesJardins.add(jardin1);
-        lesJardins.add(jardin2);
-        
-        Lpa lpa = new Lpa(lesChocolats, lesJardins);
-        
+	   
+	    Enfant enfant1 = new Enfant("Alice", 25);
+	    Enfant enfant2 = new Enfant("Bob", 30);
+	    Enfant enfant3 = new Enfant("Charlie", 35);
+
+	   
+	    Jardin jardin1 = new Jardin("142 rue Emile");
+	    jardin1.ajouteEnfant(enfant1);
+	    jardin1.ajouteEnfant(enfant2);
+
+	    Jardin jardin2 = new Jardin("2 rue Quasimir");
+	    jardin2.ajouteEnfant(enfant3);
+
+	    ArrayList<Jardin> lesJardins = new ArrayList<>();
+	    lesJardins.add(jardin1);
+	    lesJardins.add(jardin2);
+
+	    ArrayList<Chocolat> lesChocolats = new ArrayList<>();
+	    lesChocolats.add(chocolat1);
+	    lesChocolats.add(chocolat2);
+	    lesChocolats.add(chocolat3);
+
+	    return new Lpa(lesChocolats, lesJardins);
+	}
+	
+	private static void runLPA(Lpa lpa) {
         boolean finished = false;
         while (!finished) {
         	finished = true;
             if (lpa.avancer()) {
-            	Jardin currentJardin = lpa.getMonJardin();
-            	System.out.println("LPA avance à l'adresse : " + currentJardin.getAdresse());
                 lpa.deposerChocolatJardin();
                 finished = false;
-                System.out.println("LPA a fini sa tournée à l'adresse : " + currentJardin.getAdresse());
             }
         }
-        
-        System.out.println("Les LPA ont fini.");
     }
+	
+	
 }
