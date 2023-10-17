72882784235f3b4e94db7354dd3e4d138a61c747
Adrien <adriensauvage59@gmail.com>
2023-10-17 09:36:00 +0200

    class membre constructeur getteur

--- src/mediatheque/Membre.java
+++ src/mediatheque/Membre.java
@@ -1,5 +1,30 @@
 package mediatheque;
 
 public class Membre {
+	private String id, nom, prenom, adresse;
 
+	public Membre(String id, String nom, String prenom, String adresse) {
+		this.id = id;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.adresse = adresse;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public String getAdresse() {
+		return adresse;
+	}
+	
+	
 }
