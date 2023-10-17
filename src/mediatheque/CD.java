d443a0a38db97995bf626fecb9b1d4ff4ffc66b1
Adrien <adriensauvage59@gmail.com>
2023-10-17 09:51:23 +0200

    class CD initialisée

--- src/mediatheque/CD.java
+++ src/mediatheque/CD.java
@@ -1,5 +1,27 @@
 package mediatheque;
 
-public class CD {
+import java.util.Date;
 
+public class CD {
+	private String code, titre, style, artiste;
+	private Date retour;
+	private Membre emprunteur;
+	
+	public boolean estEmprunte() {
+		return emprunteur!=null;
+	}
+	
+	public void emprunter(Membre m) {
+		emprunteur = m;
+		retour = Date.currentDate().addDays(6);
+	}
+	
+	public void rendre() {
+		emprunteur = null;
+		retour = null;
+	}
+	
+	public boolean doitEtreRendu() {
+		return Date.currentDate().compareTo(retour) <= 0;
+	}
 }
