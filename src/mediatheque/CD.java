22bfe9ddb6b14192745be86f5cce23c38227cf61
Adrien <adriensauvage59@gmail.com>
2023-10-17 11:42:18 +0200

    method peutEmprunter()

--- src/mediatheque/CD.java
+++ src/mediatheque/CD.java
@@ -7,23 +7,76 @@ public class CD {
 	private Date retour;
 	private Membre emprunteur;
 	
+	
+	
+	public CD(String code, String titre, String style, String artiste, Date retour, Membre emprunteur) {
+		this.code = code;
+		this.titre = titre;
+		this.style = style;
+		this.artiste = artiste;
+		this.retour = retour;
+		this.emprunteur = emprunteur;
+	}
+	
+	
+
+	public String getCode() {
+		return code;
+	}
+
+
+
+	public String getTitre() {
+		return titre;
+	}
+
+
+
+	public String getStyle() {
+		return style;
+	}
+
+
+
+	public String getArtiste() {
+		return artiste;
+	}
+
+
+
+	public Date getRetour() {
+		return retour;
+	}
+
+
+
+	public Membre getEmprunteur() {
+		return emprunteur;
+	}
+
+
+
 	public boolean estEmprunte() {
 		return emprunteur!=null;
 	}
+	
 	
 	public void emprunter(Membre m) {
 		emprunteur = m;
 		retour = Date.currentDate().addDays(6);
 	}
 	
+	
 	public void rendre() {
 		emprunteur = null;
 		retour = null;
 	}
 	
+	
 	public boolean doitEtreRendu() {
 		return Date.currentDate().compareTo(retour) <= 0;
 	}
+	
 	
 	
 }
--- src/mediatheque/Mediatheque.java
+++ src/mediatheque/Mediatheque.java
@@ -6,8 +6,20 @@ public class Mediatheque {
 	private Set<CD> cds;
 	private Set<Membre> membres;
 	
-	public boolean peutEmprunter (Membre m);
-	public void emprunter (Membre m, CD cd);
-	public Set<CD> chercherParArtiste (String artiste);
-	public Set<CD> chercherParMotCle (String...strings motsClesDansLeTitre);
+	public boolean peutEmprunter(Membre m) {
+	    int empruntsActifs = 0;
+
+	    for (CD cd : cds) {
+	        if (cd.getEmprunteur() != null && cd.getEmprunteur().equals(m) && !cd.doitEtreRendu()) {
+	            empruntsActifs++;
+	        }
+	    }
+
+	    return empruntsActifs < 3;
+	}
+
+	
+	public void emprunter (Membre m, CD cd) {};
+	public Set<CD> chercherParArtiste (String artiste){};
+	public Set<CD> chercherParMotCle (String... motsClesDansLeTitre){};
 }
