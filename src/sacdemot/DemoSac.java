0b09286b78138469c7b34799b58ae66ce666927a
Adrien <adriensauvage59@gmail.com>
2023-10-18 14:20:09 +0200

    Ajouter mot à sac, ok

---
+++ src/sacdemot/DemoSac.java
@@ -0,0 +1,12 @@
+package sacdemot;
+
+public class DemoSac {
+	public static void main(String[] a) {
+		
+		SacDeMots sac1 = new SacDeMots();
+		
+		sac1.ajouteUnMot("Soleil");
+		
+		System.out.println(sac1.toString());
+	}
+}
--- src/sacdemot/SacDeMots.java
+++ src/sacdemot/SacDeMots.java
@@ -1,5 +1,23 @@
 package sacdemot;
 
+import java.util.*;
+
 public class SacDeMots {
+	
+	private Map<String, Integer> motOccurrences;
+
+	public SacDeMots() {
+		motOccurrences = new HashMap<>();
+	}
+	
+	public void ajouteUnMot(String mot) {
+        motOccurrences.put(mot, motOccurrences.getOrDefault(mot, 0) + 1);
+    }
+
+	@Override
+	public String toString() {
+		return "SacDeMots [motOccurrences=" + motOccurrences + "]";
+	}
+	
 	
 }
